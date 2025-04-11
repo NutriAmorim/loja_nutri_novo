21a1907fe8a21d43105a0cc26b31db542ed6e54a
marcos <nutriamorim69@gmail.com>
2025-04-11 19:11:34 -0300

    Corrige funcionamento do botão 'Entrar' na tela de login em dispositivos móveis e ajusta sidebar para fechar ao clicar nos links

--- loja_nutri/settings.py
+++ loja_nutri/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
 ]
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_ALLOWED_TEMPLATE_PACKS = ["bootstrap5"]
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Middleware
