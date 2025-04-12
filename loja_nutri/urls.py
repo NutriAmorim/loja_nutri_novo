3fb27e0e034cf1acfd0f6a64f5d223b71d80fab3
marcos <nutriamorim69@gmail.com>
2025-04-12 15:54:08 -0300

    Corrige configuração de URLs para HTTPS e ajustes no sitemap

--- loja_nutri/urls.py
+++ loja_nutri/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
 
 # Adicionando a configuração para servir arquivos estáticos e de mídia
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
